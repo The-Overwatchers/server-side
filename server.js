3918e6ca16d11f3c6cd3b56c55852380533ef7f8
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-07-30 09:46:11 -0700

    initialized server side

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const express = require('express');
+const cors = require('cors');
+const pg = require('pg');
+const app = express();
+const PORT = process.env.PORT;
+const conString = process.env.DATABASE_URL;
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', err => {
+  console.error(err);
+});
+app.use(cors());
+
+app.get('*', (req, res) => {
+  res.status(404).send('File Not Found!');
+});
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
