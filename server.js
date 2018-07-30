508d2c88ed8ab4baa18cbe95d64f7099e2466bdf
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-07-30 14:23:26 -0700

    removed old key--- OOPS

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT;
 const conString = process.env.DATABASE_URL;
 const client = new pg.Client(conString);
 const igdb = require('igdb-api-node').default;
-const igdbClient = igdb('1b6d98357aa029cadab62da1bf4c7061');
+const igdbClient = igdb();
 
 client.connect();
 client.on('error', err => {
@@ -19,18 +19,23 @@ app.use(cors());
 
 
 
-igdbClient.games({
+
+app.get('/api/v1/games/:name', (request, response) => {
+  igdbClient.games({
     fields: '*', // Return all fields
     limit: 5, // Limit to 5 results
-    offset: 15 // Index offset for results
-  }).then(response => {
-    console.log(response);
-  }).catch(error => {
-    throw error;
-});
+    search: `${request.params.name}`
+  }, [
+      'name'
+  ])
+    .then(result => response.send(result.body))
+    .catch(error => {
+      throw error;
+  });
+})
 
 
-// Maxs key: 1b6d98357aa029cadab62da1bf4c7061
+
 app.get('*', (req, res) => {
   res.status(404).send('File Not Found!');
 });
