0c712aa562d0790f5e40982a9c367a768227b339
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-07-31 11:06:20 -0700

    Publishers get pulled for description

--- server.js
+++ server.js
@@ -24,38 +24,57 @@ app.get('/api/v1/games/:name', (request, response) => {
   console.log('The search function is hitting')
   igdbClient.games({
     fields: '*', // Return all fields
-    limit: 25, // Limit to 5 results
+    limit: 3, // Limit to 5 results
     search: `${request.params.name}`
   }, [
       'name'
   ]).then(result => {
       return response.send(result.body)})
     .catch(error => {
-      throw error;
+      console.error(error);
   });
   
 })
 
 app.get('/api/v1/game-description/:id', (request, response) => {
-  console.log(request.params.id);
-  igdbClient.games({
-    ids: [
-        request.params.id
-    ]
-}, [
-    'name',
-    'cover',
-    'summary',
-    'genres',
-    'publishers',
-    'platforms'
-]).then(result => {
-      return response.send(result.body)})
-    .catch(error => {
-      throw error;
+    console.log(request.params.id);
+    // MAX -- make an object to hold all game info, then append the various things that need to be requested (platforms, genres, etc.) from their respective igdb databases
+    let gameInfo = {};
+    igdbClient.games({
+      ids: [
+          request.params.id
+      ]
+  }, [
+      'name',
+      'cover',
+      'summary',
+      'genres',
+      'themes',
+      'publishers',
+      'platforms'
+  ])
+    .then(result => {
+        console.log(result.body[0].publishers)
+        igdbClient.companies({
+          ids: result.body[0].publishers
+      }, [
+          'name'
+      ])
+        .then(publisherNames => {
+          console.log(publisherNames)
+          result.body[0].publishers = []
+          publisherNames.body.forEach((element, index) => {
+            result.body[0].publishers.push(publisherNames.body[index].name)
+          })
+          console.log(result.body[0].publishers)
+        return response.send(result.body)})
+      .catch(error => {
+      console.error(error);
   });
-  
+  })
 })
+
+
 
 
 app.get('*', (req, res) => {
